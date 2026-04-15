b511e5970d31f3a26c0209f8b4caac7b1787dcbb
amal198704-hazel <amal198704@gmail.com>
2026-04-15 20:30:06 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,38 @@
-self.addEventListener('install', (e) => {
+const CACHE_NAME = "wa-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open('wa-v1').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (e) => {
+// Activate (cleanup old cache)
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Fetch (offline support)
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then(
+      (res) => res || fetch(e.request)
+    )
   );
 });
